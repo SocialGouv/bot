5390bbd45d7cb12d150063e0e32c103c6944d7e3
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-16 03:49:20 +0200

    hooks/createNewBranchFromNewIssues: create

---
+++ src/hooks/createNewBranchFromNewIssues.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-case-declarations */
+
+const { GITHUB_AUTHOR_ASSOCIATION } = require('../constants');
+
+module.exports = async function createNewBranchFromNewIssues(context) {
+  const {
+    id, github, name, payload,
+  } = context;
+  this.log(`[${name}] [#${id}] Event received`);
+
+  try {
+    if (!payload.comment.body.startsWith('!branch')) return;
+
+    switch (payload.comment.author_association) {
+      case GITHUB_AUTHOR_ASSOCIATION.COLLABORATOR:
+      case GITHUB_AUTHOR_ASSOCIATION.OWNER:
+        this.log(`[${name}] [#${id}] Creating new branch for issue ${payload.issue.html_url}`);
+
+        const defaultBranch = context.payload.repository.default_branch;
+        const owner = context.payload.repository.owner.login;
+        const repo = context.payload.repository.name;
+        const newBranch = payload.issue.title
+          .toLowerCase()
+          .replace(/[^a-z0-9]/g, '-')
+          .replace(/-+/g, '-')
+          .replace(/^-|-$/, '');
+
+        // http://octokit.github.io/rest.js/#octokit-routes-repos-get-branch
+        const {
+          data: {
+            commit: defaultBranchCommit,
+          },
+        } = await github.repos.getBranch({ branch: defaultBranch, owner, repo });
+
+        // http://octokit.github.io/rest.js/#octokit-routes-git-create-ref
+        // https://stackoverflow.com/a/9513594/2736233
+        this.log({
+          owner, ref: `refs/heads/${newBranch}`, repo, sha: defaultBranchCommit.sha,
+        });
+        await github.git.createRef({
+          owner, ref: `refs/heads/${newBranch}`, repo, sha: defaultBranchCommit.sha,
+        });
+
+        // http://octokit.github.io/rest.js/#octokit-routes-issues-create-comment
+        await github.issues.createComment(
+          context.issue({
+            body: `Branch \`${newBranch}\` created. You can now run:\n\n    git fetch -p && git checkout ${newBranch}.`,
+          }),
+        );
+        break;
+
+      case GITHUB_AUTHOR_ASSOCIATION.NONE:
+      default:
+        return;
+    }
+  } catch (err) {
+    this.log.error(`[${name}] [#${id}] ERROR: ${err.message}`);
+    this.log.debug(err);
+  }
+};
