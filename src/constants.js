1722c5d212e08b6c57e47e0c0dc254ce40f2a19c
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-16 03:41:00 +0200

    constants: add GITHUB_AUTHOR_ASSOCIATION

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // https://developer.github.com/v3/activity/events/types/#issuecommentevent
+  GITHUB_AUTHOR_ASSOCIATION: {
+    COLLABORATOR: 'COLLABORATOR',
+    NONE: 'NONE',
+    OWNER: 'OWNER',
+  },
+};
